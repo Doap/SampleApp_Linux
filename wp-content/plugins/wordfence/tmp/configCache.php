a4caf736c04dc0217103e041e7924e80b79268f3
David Menache <dave@doap.com>
2015-01-06 05:27:15 +0000

    deleted a pile of zips

--- wp-content/plugins/wordfence/tmp/configCache.php
+++ wp-content/plugins/wordfence/tmp/configCache.php
@@ -1,4 +1,4 @@
 <?php
 /* Wordfence temporary file security header */
 echo "Nothing to see here!\n"; exit(0);
-?>a:11:{s:9:"cacheType";s:0:"";s:15:"firewallEnabled";s:1:"0";s:18:"liveTrafficEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";s:6:"apiKey";s:128:"a422d9b2b04e33c9c1d4c43d431ed713692722c8d89ee320c7e6fbe9d0220ef5087e19990519cd73300816f51a87de67070c57f0425be479b557dc5d426c803f";s:18:"liveTraf_ignoreIPs";s:0:"";s:17:"liveTraf_ignoreUA";s:0:"";s:9:"howGetIPs";s:0:"";s:19:"other_hideWPVersion";s:1:"1";s:6:"encKey";s:16:"7cb09df2283007d5";s:20:"loginSec_lockoutMins";s:2:"30";}
+?>a:13:{s:9:"cacheType";s:0:"";s:15:"firewallEnabled";s:1:"0";s:18:"liveTrafficEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";s:6:"apiKey";s:128:"a422d9b2b04e33c9c1d4c43d431ed713692722c8d89ee320c7e6fbe9d0220ef5087e19990519cd73300816f51a87de67070c57f0425be479b557dc5d426c803f";s:18:"liveTraf_ignoreIPs";s:0:"";s:17:"liveTraf_ignoreUA";s:0:"";s:9:"howGetIPs";s:0:"";s:19:"other_hideWPVersion";s:1:"1";s:6:"encKey";s:16:"7cb09df2283007d5";s:21:"scheduledScansEnabled";s:1:"1";s:22:"lastScheduledScanStart";s:10:"1420520614";s:11:"other_WFNet";s:1:"1";}
